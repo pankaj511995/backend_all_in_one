c8f82dbd2f4fb170fd7c5dd833afb71b818b644d
pankaj511995 <kpankaj511995@gmail.com>
2023-06-04 09:39:35 +0530

    t

--- authentication/authenticate.js
+++ authentication/authenticate.js
@@ -3,7 +3,7 @@ const Muser=require('../models/MongoUser')
 const {error,verify}=require('../service/repete')
 
 exports.authenticate= async(req,res,next)=>{
-    // try{ 
+    try{ 
        const token=await verify(req.headers.authorization,process.env.JWT_TOKEN)
 
        if(!token) throw new Error('')
@@ -13,9 +13,9 @@ exports.authenticate= async(req,res,next)=>{
             next()
            }else throw new Error('')
                           
-    // }catch(err){ 
-    //     console.log(err.message)
-    //     error(res,'user does not exist','error while authentication')
-    // }
+    }catch(err){ 
+        console.log(err.message)
+        error(res,'user does not exist','error while authentication')
+    }
 
 }
